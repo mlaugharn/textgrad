60a2017d168eb1c5b68f8093229c6e1310447b2c
Marc Laugharn <mlaugharn@gmail.com>
2024-06-13 20:16:17 -0700

    model string fix

--- textgrad/engine/vllm.py
+++ textgrad/engine/vllm.py
@@ -10,6 +10,7 @@ class ChatVLLM(ChatOpenAI):
     DEFAULT_SYSTEM_PROMPT = "You are a helpful, creative, and smart assistant."
         
     def __init__(self, model_name_or_path, system_prompt=DEFAULT_SYSTEM_PROMPT, **kwargs):
+        self.model_string = model_name_or_path
         root = platformdirs.user_cache_dir("textgrad")
         cache_path = os.path.join(root, f"cache_vllm_{model_name_or_path}.db")
         CachedEngine.__init__(self, cache_path)
@@ -17,7 +18,7 @@ class ChatVLLM(ChatOpenAI):
         # Set OpenAI's API key and API base to use vLLM's API server.
         openai_api_key = "EMPTY"
         openai_api_base = "http://localhost:8000/v1"
-
+        
         self.client = OpenAI(
             api_key=openai_api_key,
             base_url=openai_api_base,
